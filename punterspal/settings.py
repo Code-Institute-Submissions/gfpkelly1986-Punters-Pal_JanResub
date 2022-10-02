31564a0e3b9e219e88f57185b522701845fbef57
gfpkelly1986 <gfpkelly@gmail.com>
2022-10-02 15:40:08 +0000

    Fix typo in Heroku hostname

--- punterspal/settings.py
+++ punterspal/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['punters-pal.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['punterspal.herokuapp.com', 'localhost']
 
 
 # Application definition
