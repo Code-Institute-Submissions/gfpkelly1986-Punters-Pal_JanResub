c4dae14157d12ecde04e6c67833d5896530f3534
gfpkelly1986 <gfpkelly@gmail.com>
2022-10-02 15:59:39 +0000

    Add test to views to check HTTP response OK

--- punterspal/urls.py
+++ punterspal/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from diary import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.say_hello, name='test-httpresponse')
 ]
