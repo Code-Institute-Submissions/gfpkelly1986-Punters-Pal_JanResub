0057c2edeb41ab7776eee55180f8e06a71821545
gfpkelly1986 <gfpkelly@gmail.com>
2022-10-08 10:12:25 +0000

    Change view URL and re-test in Heroku.

--- diary/views.py
+++ diary/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def get_landing_page(request):
-    return render(request, 'diary/templates/index.html')
+    return render(request, 'index.html')
