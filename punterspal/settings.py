ce36492d6e6ae4b8bf8e7df906bbddc2f5b13c46
gfpkelly1986 <gfpkelly@gmail.com>
2023-01-08 10:15:50 +0000

    Remove debug config var with backup set to false.

--- punterspal/settings.py
+++ punterspal/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEVELOPMENT', False)
 
 ALLOWED_HOSTS = ['punters-pal.herokuapp.com', 'localhost']
 
